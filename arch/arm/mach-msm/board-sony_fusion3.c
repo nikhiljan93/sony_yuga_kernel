6de0f507d6f95237250f60f4fa32aec2484f8cee
motley <motley.slate@gmail.com>
2013-02-16 20:14:49 -0500

    msm thermal: enhancements
    1) Smart polling based on temp (poll slower when cooler, faster when
    warmer).
    
    2) Added low frequency index so we don't throttle too low and kill
    performance. When warming normally (throttle temp is reached) it will
    step down by the freq_step until back under control. The lowest
    frequency step possible is the specified by the low freq index.
    
    3) Added throttle max (hardcoded at 80C). If it reaches the
    throttle max, it will immediately throttle down to the min_freq_index
    instead of stepping down 2 frequency steps at a time like when it hits
    the throttle temp.
    
    4) Added tuneables
    
    /sys/module/msm_thermal/parameters/throttle_temp
    /sys/module/msm_thermal/parameters/min_freq_index
    /sys/module/msm_thermal/parameters/thermal_debug

--- arch/arm/mach-msm/board-sony_fusion3.c
+++ arch/arm/mach-msm/board-sony_fusion3.c
@@ -3170,12 +3170,8 @@ static struct platform_device msm_tsens_device = {
 
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 7,
- 	.poll_ms = 250,
-#ifdef CONFIG_CPU_OVERCLOCK
+ 	.poll_ms = 500,
 	.limit_temp_degC = 70,
-#else
-	.limit_temp_degC = 60,
-#endif
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 #ifdef CONFIG_INTELLI_THERMAL
