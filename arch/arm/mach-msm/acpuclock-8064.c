538a409b97e714eba7f533e31dee5ad509057b66
ktoonsez <ktoonsez@gmail.com>
2013-05-29 15:22:57 -0700

    acpuclock: Try 4600 on BUS bandwidth
    Conflicts:
    
    	arch/arm/mach-msm/acpuclock-8064.c

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -107,6 +107,7 @@ static struct msm_bus_paths bw_level_tbl[] __initdata = {
 	[3] = BW_MBPS(2128), /* At least 266 MHz on bus. */
 	[4] = BW_MBPS(3200), /* At least 400 MHz on bus. */
 	[5] = BW_MBPS(4264), /* At least 533 MHz on bus. */
+	[6] = BW_MBPS(4600), /* At least 600 MHz on bus. */
 };
 
 static struct msm_bus_scale_pdata bus_scale_data __initdata = {
@@ -134,9 +135,9 @@ static struct l2_level l2_freq_tbl[] __initdata = {
 	[14] = { { 1134000, HFPLL, 1, 0x2A }, 1150000, 1150000, 5 },
 	[15] = { { 1188000, HFPLL, 1, 0x2C }, 1150000, 1150000, 5 },
 #ifdef CONFIG_CPU_OVERCLOCK
-	[16] = { { 1242000, HFPLL, 1, 0x2E }, 1175000, 1175000, 5 },
-	[17] = { { 1296000, HFPLL, 1, 0x30 }, 1175000, 1175000, 5 },
-	[18] = { { 1350000, HFPLL, 1, 0x32 }, 1175000, 1175000, 5 },
+	[16] = { { 1242000, HFPLL, 1, 0x2E }, 1175000, 1175000, 6 },
+	[17] = { { 1296000, HFPLL, 1, 0x30 }, 1175000, 1175000, 6 },
+	[18] = { { 1350000, HFPLL, 1, 0x32 }, 1175000, 1175000, 6 },
 #endif
 	{ }
 };
