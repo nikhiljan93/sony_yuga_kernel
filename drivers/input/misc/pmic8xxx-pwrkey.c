f47442fa6aa865a69e2e33ac6aa894056552dc39
nikhiljan93 <nikhil.jan93@gmail.com>
2013-09-07 12:08:36 +0530

    Defconfig updates

--- drivers/input/misc/pmic8xxx-pwrkey.c
+++ drivers/input/misc/pmic8xxx-pwrkey.c
@@ -218,7 +218,6 @@ static int __devinit pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 	INIT_DELAYED_WORK(&pwrkey->confirm_work, confirm_key_status);
 
 	platform_set_drvdata(pdev, pwrkey);
-
 	err = request_any_context_irq(key_press_irq, pwrkey_press_irq,
 		IRQF_TRIGGER_RISING, "pmic8xxx_pwrkey_press", pwrkey);
 	if (err < 0) {
