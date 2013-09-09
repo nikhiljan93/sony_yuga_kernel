ccc3b498f322e8f389f4cebb00c5defa38d2f3e4
anarkia1976 <stefano.villa1976@gmail.com>
2013-09-09 16:14:01 +0200

    auto_hotplug: tuning performance

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -47,7 +47,7 @@
  * SAMPLING_PERIODS * MIN_SAMPLING_RATE is the minimum
  * load history which will be averaged
  */
-#define DEFAULT_SAMPLING_PERIODS	10
+#define DEFAULT_SAMPLING_PERIODS	8
 
 /*
  * DEFAULT_MIN_SAMPLING_RATE is the base minimum sampling rate
@@ -63,9 +63,9 @@
  * DEFAULT_DISABLE_LOAD_THRESHOLD is the default load at which a CPU is disabled
  * These two are scaled based on num_online_cpus()
  */
-#define DEFAULT_ENABLE_ALL_LOAD_THRESHOLD	(100 * CPUS_AVAILABLE)
-#define DEFAULT_ENABLE_LOAD_THRESHOLD		200
-#define DEFAULT_DISABLE_LOAD_THRESHOLD		80
+#define DEFAULT_ENABLE_ALL_LOAD_THRESHOLD	(120 * CPUS_AVAILABLE)
+#define DEFAULT_ENABLE_LOAD_THRESHOLD		160
+#define DEFAULT_DISABLE_LOAD_THRESHOLD		95
 
 #define SUSPEND_FREQ 702000
 
