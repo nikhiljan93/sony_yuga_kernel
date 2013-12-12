3f970712d3deb175b676c1cee1335f95b894710d
nikhiljan93 <nikhil.jan93@gmail.com>
2013-12-13 02:20:21 +0530

    XzK-v16 Beta 4

--- arch/arm/mach-msm/simple_plug.c
+++ arch/arm/mach-msm/simple_plug.c
@@ -25,16 +25,15 @@
 #include <linux/cpufreq.h>
 
 #define SIMPLE_PLUG_MAJOR_VERSION	1
-#define SIMPLE_PLUG_MINOR_VERSION	1
+#define SIMPLE_PLUG_MINOR_VERSION	2
 
-#define STARTUP_DELAY_MS		5000
+#define STARTUP_DELAY_MS		100000
 #define DEF_SAMPLING_MS			10
 #define DEF_VERIFY_MS			120000	/* 3 mins on average to fix */
 #define HISTORY_SIZE			10
 #define NUM_CORES			4
 
 static struct delayed_work simple_plug_work;
-static struct work_struct set_max_frequency_work[NUM_CORES];
 
 static unsigned int simple_plug_active = 1;
 static unsigned int min_cores = 1;
@@ -61,8 +60,8 @@ static unsigned int nr_last_i;
  * first invocation, this replaces the need for special startup checking
  * of cores.
  */
-static unsigned int n_until_verify;
-static bool verify_needed = false;
+static unsigned int n_until_verify = 1;
+static bool verify_needed = true;
 static unsigned int n_online;
 
 module_param(simple_plug_active, uint, 0644);
@@ -122,9 +121,8 @@ static unsigned __cpuinit desired_number_of_cores(void)
 }
 
 static void
-set_max_frequency(struct work_struct *work)
+set_max_frequency(int cpu)
 {
-	int cpu = work - &set_max_frequency_work[0];
 	struct cpufreq_policy policy;
 
 	if (cpufreq_get_policy(&policy, cpu) < 0) {
@@ -175,7 +173,7 @@ static void __cpuinit cpus_up_down(int desired_n_online)
 			pr_debug(PR_NAME "starting cpu%d, want %d online\n", cpu, desired_n_online);
 			STATS(times_core_up[cpu]++);
 			cpu_up(cpu);
-			schedule_work_on(cpu, &set_max_frequency_work[cpu]);
+			set_max_frequency(cpu);
 		} else if (cpu >= desired_n_online && cpu < n_online) {
 			pr_debug(PR_NAME "unplugging cpu%d, want %d online\n", cpu, desired_n_online);
 			STATS(times_core_down[cpu]++);
@@ -274,8 +272,6 @@ static struct early_suspend simple_plug_early_suspend_struct_driver = {
 
 static int __init simple_plug_init(void)
 {
-	int cpu;
-
 	pr_info(PR_NAME "version %d.%d by crpalmer\n",
 		 SIMPLE_PLUG_MAJOR_VERSION,
 		 SIMPLE_PLUG_MINOR_VERSION);
@@ -289,14 +285,16 @@ static int __init simple_plug_init(void)
 	 * is initialized, it will crash.  The race window seems to be in the
 	 * order 10s of ms, so 5 seconds gives tons of time for it to
 	 * resolve itself.
+	 *
+	 * In addition to the need for this hack, let's let the system get
+	 * itself booted up as quickly as possible.  Instead of trying to
+	 * plug/unplug as we boot, wait long enough for the system to
+	 * stabilize.
+	 *
+	 * We will then immediately force the state that we want onto the system.
 	 */
 	
 	INIT_DELAYED_WORK(&simple_plug_work, simple_plug_work_fn);
-
-	for (cpu = 0; cpu < NUM_CORES; cpu++) {
-		INIT_WORK(&set_max_frequency_work[cpu], set_max_frequency);
-	}
-
 	schedule_delayed_work_on(0, &simple_plug_work, msecs_to_jiffies(STARTUP_DELAY_MS));
 
 	return 0;
